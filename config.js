7019f2154021f383fa577e475d8c486c019e48d7
xlkkssss2000 <213364331+xlkkssss7777@users.noreply.github.com>
2026-01-17 15:49:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'ur3267589-8f0b05c0ab405dece1d597f8',
-    //'m797638073-924f5efed86446cce1c95ae1', // 周润发的博客 API key
+    //'m802163742-70db4a866e774e1b6e06503f', // Firefox API key
     //'m797713213-1c83620d56db599960ad0244', // 周润发的公益订阅 API key
     //'m797713331-835664f191b589688f122e00', // 周润发的公益订阅器 API key
   ],
@@ -34,5 +34,6 @@ window.Config = {
     },
   ],
 };
+
 
 
