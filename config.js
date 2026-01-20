1dd2aacd16d6e9c7c6110e90b4a42b44b8ab307e
xlkkssss2000 <213364331+xlkkssss7777@users.noreply.github.com>
2026-01-20 22:29:02 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ window.Config = {
   ApiKeys: [
     'ur3267589-8f0b05c0ab405dece1d597f8',
     'ur2895179-3f53714943878fe8f96aabc2',
+    'uk1_E1UErZ48qqQDnXyXrD3qfxdRin0UF__9bJX2rlYF,
     //'m802163742-70db4a866e774e1b6e06503f', // Firefox API key
     //'m802164088-f68baad8b791abd60b1683e3', // 影视 API key
     //'m802164114-bb9176ad9b506fa5a682778b', // Uptime Kuma API key
@@ -55,6 +56,7 @@ window.Config = {
     },
   ],
 };
+
 
 
 
